e357f503733e346cab92a066b87a4f68a7e87c0f
1066324848 <1066324848@qq.com>
2023-08-08 17:38:53 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img src="./assets/img/jianLi.png" alt="">
+</template>
+         
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+img{
+  width: 60%;
+  margin: 0 auto;
+  display: block;
+}
+</style>
